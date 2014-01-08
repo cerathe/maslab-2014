41c6f6def2fcaccccf88e417b5d29f7d0c136fc3
Felix Sun <fephsun@gmail.com>
2014-01-08 12:45:04 -0500

    Draws a rectangle around green ball.

--- opencv/java-opencv-test/src/ImageProcessor.java
+++ opencv/java-opencv-test/src/ImageProcessor.java
@@ -1,13 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
-import org.opencv.core.Size;
+import org.opencv.core.Point;
 import org.opencv.imgproc.Imgproc;
 
 
 public class ImageProcessor {
 	static Scalar greenFilterLower = new Scalar(40, 60, 60);
 	static Scalar greenFilterUpper = new Scalar(80, 255, 255);
+	static double OK_RATIO = 2.0;
+	static double MIN_FILL_PROPORTION = 0.2;
+	static Scalar GREEN = new Scalar(0, 255, 0);
 	
 	static {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
@@ -20,13 +28,48 @@ public class ImageProcessor {
 	//  the output image will be for your visual reference,
 	//  but you will mainly want to output a list of the locations of detected objects.)
 	public static Mat process(Mat rawImage) {
+		// Convert to HSV
 		Mat hsvImage= new Mat();
 		Imgproc.cvtColor(rawImage, hsvImage, Imgproc.COLOR_BGR2HSV);
+		// Find green stuff
 		Mat greenMask = new Mat();
 		Core.inRange(hsvImage, greenFilterLower, greenFilterUpper, greenMask);
-		Mat output = new Mat();
-		Imgproc.cvtColor(greenMask, output, Imgproc.COLOR_GRAY2BGR);
-		return output;
+		// Find blobs of green
+		List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+		Mat hierarchy = new Mat();
+		Imgproc.findContours(greenMask, contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_NONE);
+		// Look for the "best" blob
+		// The best blob is the largest one that is also
+		// - Roughly square / circular
+		// = Filled enough
+		int bestBlob = -1;
+		Rect bestBoundingRect = new Rect();
+		double bestArea = 0;
+		for (int i = 0; i < contours.size(); i++) {
+			// Make a rectangle.
+			Rect boundingRect = Imgproc.boundingRect(contours.get(i));
+			// Is the rectangle roughly square?
+			double height_ratio = 1.0 * boundingRect.width / boundingRect.height;
+			if (! ((1.0 / OK_RATIO < height_ratio) && (height_ratio < OK_RATIO))){
+				continue;
+			}
+			// Is the rectangle filled enough?
+			double blobArea = Imgproc.contourArea(contours.get(i));
+			if (1.0 * blobArea / boundingRect.area() < MIN_FILL_PROPORTION){
+				continue;
+			}
+			// If we get this far, we are good.
+			if (Imgproc.contourArea(contours.get(i)) > bestArea){
+				bestBlob = i;
+				bestArea = blobArea;
+				bestBoundingRect = boundingRect;
+			}
+		}
+		// Display the ball we found.
+		Core.rectangle(rawImage, bestBoundingRect.tl(), bestBoundingRect.br(), GREEN);
+//		Mat output = new Mat();
+//		Imgproc.cvtColor(greenMask, output, Imgproc.COLOR_GRAY2BGR);
+		return rawImage;
 	}
 	
 }
