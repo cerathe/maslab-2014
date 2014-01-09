ddf22b4f41dc77dfae487e02ebce66bb2e366037
Felix Sun <fephsun@gmail.com>
2014-01-09 19:28:10 +0000

    First version of ball-following script

---
+++ Derpbot/src/edu/mit/felixsun/maslab/ImageProcessor.java
@@ -0,0 +1,114 @@
+package edu.mit.felixsun.maslab;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.imgproc.Imgproc;
+
+
+public class ImageProcessor {
+	static int greenLowerH = 40;
+	static int greenUpperH = 80;
+	static int redLowerH = 170;
+	static int redUpperH = 10;
+	static int lowerS = 120;
+	static int lowerV = 40;
+	static double OK_RATIO = 2.0;
+	static double MIN_FILL_PROPORTION = 0.2;
+	static Scalar GREEN = new Scalar(0, 255, 0);
+	
+	static {
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+	}
+	
+	static Mat colorFilter(Mat input, int lowerH, int upperH){
+		/*
+		 * Finds all pixels of input image (in HSV form) that have hue between 
+		 * lowerH and upperH.  Is smart about H wrap-around: for example, if
+		 * lowerH = 170 and upperH = 10, it will find lowerH between 170 and 180;
+		 * as well as between 180 and 10.
+		 */
+		Mat output = new Mat();
+		Scalar lowerHSV = new Scalar(lowerH, lowerS, lowerV);
+		Scalar upperHSV = new Scalar(upperH, 255, 255);
+		if (upperH > lowerH) {
+			Core.inRange(input, lowerHSV, upperHSV, output);
+		} else {
+			Mat part1 = new Mat();
+			Mat part2 = new Mat();
+			Core.inRange(input, lowerHSV, new Scalar(180, 255, 255), part1);
+			Core.inRange(input, new Scalar(0, lowerS, lowerV), upperHSV, part2);
+			Core.bitwise_or(part1, part2, output);
+		}
+		return output;
+	}
+
+	// Input: an image from the camera, an empty mat to store an output image (for
+	// visual debugging only).
+	// Output: A cvData structure, containing data that the main controller wants to know.
+	public static cvData process(Mat rawImage, Mat processedImage) {
+		// Convert to HSV
+		Mat hsvImage= new Mat();
+		Imgproc.cvtColor(rawImage, hsvImage, Imgproc.COLOR_BGR2HSV);
+		// Find green stuff
+		Mat colorMask = colorFilter(hsvImage, greenLowerH, greenUpperH);
+		// Core.inRange(hsvImage, redFilterLower, redFilterUpper, colorMask);
+		
+		// Find blobs of color
+		List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
+		Mat hierarchy = new Mat();
+		Imgproc.findContours(colorMask.clone(), contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_NONE);
+		// Look for the "best" blob
+		// The best blob is the largest one that is also
+		// - Roughly square / circular
+		// = Filled enough
+		int bestBlob = -1;
+		Rect bestBoundingRect = new Rect();
+		double bestArea = 0;
+		for (int i = 0; i < contours.size(); i++) {
+			// Make a rectangle.
+			Rect boundingRect = Imgproc.boundingRect(contours.get(i));
+			// Is the rectangle roughly square?
+			double height_ratio = 1.0 * boundingRect.width / boundingRect.height;
+			if (! ((1.0 / OK_RATIO < height_ratio) && (height_ratio < OK_RATIO))){
+				continue;
+			}
+			// Is the rectangle filled enough?
+			double blobArea = Imgproc.contourArea(contours.get(i));
+			if (1.0 * blobArea / boundingRect.area() < MIN_FILL_PROPORTION){
+				continue;
+			}
+			// If we get this far, we are good.
+			if (Imgproc.contourArea(contours.get(i)) > bestArea){
+				bestBlob = i;
+				bestArea = blobArea;
+				bestBoundingRect = boundingRect;
+			}
+		}
+		// Display the ball we found.
+		Core.rectangle(rawImage, bestBoundingRect.tl(), bestBoundingRect.br(), GREEN);
+		// Also show the masked input.
+		Imgproc.cvtColor(colorMask, processedImage, Imgproc.COLOR_GRAY2BGR);
+		
+		// How far off the center of the screen is the ball?
+		double offset;
+		if (bestBlob == -1) {
+			// Actually, we couldn't even find the ball.  Report this.
+			offset = -1;
+		} else {
+			// Offset:
+			// -1 means all the way to the left
+			// 1 means all the way to the right
+			// 0 means centered
+			offset = 1.0 * (bestBoundingRect.x + bestBoundingRect.width / 2 - rawImage.width() / 2) / rawImage.width();
+		}
+		cvData outData = new cvData();
+		outData.offset = offset;
+		return outData;
+	}
+	
+}
