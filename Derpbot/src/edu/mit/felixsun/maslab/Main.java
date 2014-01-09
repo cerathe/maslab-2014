37ec534a3c12ecbfb1ba9f46aa9992ec58657583
Felix Sun <fephsun@gmail.com>
2014-01-09 16:31:33 +0000

    Fixing a version control meltdown

--- Derpbot/src/edu/mit/felixsun/maslab/Main.java
+++ Derpbot/src/edu/mit/felixsun/maslab/Main.java
@@ -1,9 +1,21 @@
 package edu.mit.felixsun.maslab;
 
+import jssc.SerialPort;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Just a testing framework for the machine vision stuff.
+		
+		while (true) {
+			
+			
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
 
 	}
 
