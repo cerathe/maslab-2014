81818fe626685e9aa7f33bf55e0cb544ccc812d7
Felix Sun <fephsun@gmail.com>
2014-01-09 19:28:10 +0000

    First version of ball-following script

--- Derpbot/src/edu/mit/felixsun/maslab/Main.java
+++ Derpbot/src/edu/mit/felixsun/maslab/Main.java
@@ -1,12 +1,116 @@
 package edu.mit.felixsun.maslab;
 
+import java.awt.BorderLayout;
+import java.awt.image.BufferedImage;
+
+import edu.mit.felixsun.maslab.ImageProcessor;
+import edu.mit.felixsun.maslab.Mat2Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+import org.opencv.core.Core;
+import org.opencv.core.Mat;
+import org.opencv.highgui.Highgui;
+import org.opencv.highgui.VideoCapture;
+
 import jssc.SerialPort;
 import jssc.SerialPortException;
+
+class cvData {
+	/*
+	 * A bunch of data that gets passed between the computer vision system
+	 * and the rest of the code.
+	 */
+	public double offset;
+	public cvData() {
+		offset = -1;
+	}
+}
+
+class cvHandle implements Runnable {
+	/*
+	 * Starts the cv scripts.  Runs in a separate thread.
+	 */
+	cvData data;
+	Thread t;
+	public cvHandle(cvData initData) {
+		data = initData;
+		t = new Thread(this);
+		t.run();
+	}
+	
+	public void run(){
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+
+		// Setup the camera
+		VideoCapture camera = new VideoCapture();
+		camera.open(1);
+		
+		// Create GUI windows to display camera output and OpenCV output
+		int width = (int) (camera.get(Highgui.CV_CAP_PROP_FRAME_WIDTH));
+		int height = (int) (camera.get(Highgui.CV_CAP_PROP_FRAME_HEIGHT));
+		JLabel cameraPane = createWindow("Camera output", width, height);
+		JLabel opencvPane = createWindow("OpenCV output", width, height);
+
+		// Main loop
+		Mat rawImage = new Mat();
+		Mat processedImage = new Mat();
+		while (true) {
+			
+			// Wait until the camera has a new frame
+			camera.grab();
+			camera.retrieve(rawImage);
+			
+			// Process the image however you like
+			cvData tempData = ImageProcessor.process(rawImage, processedImage);
+			synchronized(data) {
+				data = tempData;
+			}
+			
+			// Update the GUI windows
+			updateWindow(cameraPane, rawImage);
+			updateWindow(opencvPane, processedImage);
+			// Manually garbage collect, because opencv has memory issues :(
+			System.gc();
+			
+		}
+	}
+	
+	private static JLabel createWindow(String name, int width, int height) {    
+        JFrame imageFrame = new JFrame(name);
+        imageFrame.setSize(width, height);
+        imageFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        JLabel imagePane = new JLabel();
+        imagePane.setLayout(new BorderLayout());
+        imageFrame.setContentPane(imagePane);
+        
+        imageFrame.setVisible(true);
+        return imagePane;
+    }
+    
+    private static void updateWindow(JLabel imagePane, Mat mat) {
+    	int w = (int) (mat.size().width);
+    	int h = (int) (mat.size().height);
+    	if (imagePane.getWidth() != w || imagePane.getHeight() != h) {
+    		imagePane.setSize(w, h);
+    	}
+    	BufferedImage bufferedImage = Mat2Image.getImage(mat);
+    	imagePane.setIcon(new ImageIcon(bufferedImage));
+    }
+}
+
 
 public class Main {
 
 	public static void main(String[] args) {
-		// Just a testing framework for the machine vision stuff.
+		// Just a testing framework for the computer vision stuff.
+		cvData data = new cvData();
+		cvHandle handle = new cvHandle(data); // Run the cv stuff.
+
+		// Start serial communication.
 		SerialPort serialPort;
 		serialPort = new SerialPort("COM4");
 		try {
@@ -18,8 +122,22 @@ public class Main {
 		}
 
 		while (true) {
-			int motorA = 20;
-			int motorB = 20;
+			double offset;
+			int motorA = 0;
+			int motorB = 0;
+			synchronized(data){
+				offset = data.offset;
+			}
+			if (offset < 0){
+				// We don't see color.  Just keep spinning.
+				motorA = 20;
+				motorB = -20;
+			} else {
+				// Steer proportional to where the color is.
+				motorA = (int) (20 + offset*40);
+				motorB = (int) (20 - offset*40);
+			}
+
 			byte[] outData = new byte[4];
 			outData[0] = 'S';				// Start signal "S"
 			outData[1] = (byte) -motorA;	// Motor A data
