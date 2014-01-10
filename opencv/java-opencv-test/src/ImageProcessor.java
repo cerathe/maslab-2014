229791a4057d69dfc70b5ac0611c76b42afbfa76
Felix Sun <fephsun@gmail.com>
2014-01-10 21:11:47 +0000

    Nuked java-opencv-test project.  (All useful changes moved to derpbot_

--- opencv/java-opencv-test/src/ImageProcessor.java
+++ opencv/java-opencv-test/src/ImageProcessor.java
@@ -2,49 +2,32 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.opencv.core.Core;
+import org.opencv.core.CvType;
 import org.opencv.core.Mat;
-import org.opencv.core.MatOfPoint;
-import org.opencv.core.Rect;
-import org.opencv.core.Scalar;
-import org.opencv.core.Point;
+import org.opencv.core.Size;
 import org.opencv.imgproc.Imgproc;
 
 
 public class ImageProcessor {
-	static int greenLowerH = 40;
-	static int greenUpperH = 80;
-	static int redLowerH = 170;
-	static int redUpperH = 10;
-	static int lowerS = 120;
-	static int lowerV = 40;
-	static double OK_RATIO = 2.0;
-	static double MIN_FILL_PROPORTION = 0.2;
-	static Scalar GREEN = new Scalar(0, 255, 0);
+	
+	private static final int numBuffers = 1;
+	private List<Mat> buffers = null;
 	
 	static {
 		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
 	}
 	
-	static Mat colorFilter(Mat input, int lowerH, int upperH){
-		/*
-		 * Finds all pixels of input image (in HSV form) that have hue between 
-		 * lowerH and upperH.  Is smart about H wrap-around: for example, if
-		 * lowerH = 170 and upperH = 10, it will find lowerH between 170 and 180;
-		 * as well as between 180 and 10.
-		 */
-		Mat output = new Mat();
-		Scalar lowerHSV = new Scalar(lowerH, lowerS, lowerV);
-		Scalar upperHSV = new Scalar(upperH, 255, 255);
-		if (upperH > lowerH) {
-			Core.inRange(input, lowerHSV, upperHSV, output);
-		} else {
-			Mat part1 = new Mat();
-			Mat part2 = new Mat();
-			Core.inRange(input, lowerHSV, new Scalar(180, 255, 255), part1);
-			Core.inRange(input, new Scalar(0, lowerS, lowerV), upperHSV, part2);
-			Core.bitwise_or(part1, part2, output);
+	public ImageProcessor() {
+		// Fill "buffers" with however many intermediate images that you will need
+		// to get from "rawImage" to "processedImage" in the "process" method
+		buffers = new ArrayList<Mat>();
+		for (int i = 0; i < numBuffers; i++) {
+			buffers.add(new Mat(new Size(), CvType.CV_8UC3));
+			
+			buffers.add(new Mat(new Size(), CvType.CV_8UC1));
+			buffers.add(new Mat(new Size(), CvType.CV_8UC1));
+			buffers.add(new Mat(new Size(), CvType.CV_8UC1));
 		}
-		return output;
 	}
 
 	// Input: an image from the camera
@@ -53,56 +36,16 @@ public class ImageProcessor {
 	// (In practice it's a little different:
 	//  the output image will be for your visual reference,
 	//  but you will mainly want to output a list of the locations of detected objects.)
-	public static Mat process(Mat rawImage) {
-		// Convert to HSV
-		Mat hsvImage= new Mat();
-		Imgproc.cvtColor(rawImage, hsvImage, Imgproc.COLOR_BGR2HSV);
-		// Find green stuff
-		Mat colorMask = colorFilter(hsvImage, greenLowerH, greenUpperH);
-		// Core.inRange(hsvImage, redFilterLower, redFilterUpper, colorMask);
+	public void process(Mat rawImage, Mat processedImage) {
 		
-		// Find blobs of color
-		List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
-		Mat hierarchy = new Mat();
-		Imgproc.findContours(colorMask.clone(), contours, hierarchy, Imgproc.RETR_EXTERNAL, Imgproc.CHAIN_APPROX_NONE);
-		// Look for the "best" blob
-		// The best blob is the largest one that is also
-		// - Roughly square / circular
-		// = Filled enough
-		int bestBlob = -1;
-		Rect bestBoundingRect = new Rect();
-		double bestArea = 0;
-		for (int i = 0; i < contours.size(); i++) {
-			// Make a rectangle.
-			Rect boundingRect = Imgproc.boundingRect(contours.get(i));
-			// Is the rectangle roughly square?
-			double height_ratio = 1.0 * boundingRect.width / boundingRect.height;
-			if (! ((1.0 / OK_RATIO < height_ratio) && (height_ratio < OK_RATIO))){
-				continue;
-			}
-			// Is the rectangle filled enough?
-			double blobArea = Imgproc.contourArea(contours.get(i));
-			if (1.0 * blobArea / boundingRect.area() < MIN_FILL_PROPORTION){
-				continue;
-			}
-			// If we get this far, we are good.
-			if (Imgproc.contourArea(contours.get(i)) > bestArea){
-				bestBlob = i;
-				bestArea = blobArea;
-				bestBoundingRect = boundingRect;
-			}
-		}
-		// Display the ball we found.
-		Core.rectangle(rawImage, bestBoundingRect.tl(), bestBoundingRect.br(), GREEN);
+		// These two lines are a workaround for the fact that CvtColor throws weird errors
+		// when you try to convert from a 3-channel (BGR) image to a 1-channel (grayscale) image.
+		// The following is a workaround: convert BGR to HSV, and take only the V channel
+		// (which will end up in buffers.get(3)).
+		Imgproc.cvtColor(rawImage, buffers.get(0), Imgproc.COLOR_BGR2HSV);
+		Core.split(buffers.get(0), buffers.subList(1, 4));
 		
-		// Also show the masked input.
-		Mat output = new Mat();
-		Imgproc.cvtColor(colorMask, output, Imgproc.COLOR_GRAY2BGR);
-//		hsvImage.release();
-//		colorMask.release();
-//		hierarchy.release();
-//		contours.clear();
-		return output;
+		Imgproc.blur(buffers.get(3), processedImage, new Size(9, 9));
 	}
 	
 }
