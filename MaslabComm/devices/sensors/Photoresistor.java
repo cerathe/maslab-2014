2078669f9ccc98f9ce93d6c45e3436e0653af607
Vipul <vvachh@gmail.com>
2014-01-25 15:46:38 -0500

    Add photoresistor

---
+++ MaslabComm/devices/sensors/Photoresistor.java
@@ -0,0 +1,36 @@
+package devices.sensors;
+
+import devices.actuators.DigitalOutput;
+
+public class Photoresistor extends AnalogInput{
+
+	private DigitalOutput led;
+	private float MinValue = 3;
+	private float ThreshValue = 10;
+	public Photoresistor(int i) {
+		super(i);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public float getValue(){
+		return this.value;
+	}
+
+	public boolean isBall(){
+		boolean output = false;
+		led.setValue(true);
+		if(this.value>MinValue){
+			output = true;
+		}
+		return output;
+	}
+	
+	public boolean isRed(){
+		boolean output = false;
+		if(this.value<ThreshValue){
+			output =true;
+		}
+		return output;
+	}
+
+}
