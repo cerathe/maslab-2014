eea996c290497bc01f0f8b0ac02bc3ef460383ed
Vipul <vvachh@gmail.com>
2014-01-29 16:25:52 -0500

    BallSortState checks if it exists before doing anything

--- Derpbot/src/edu/mit/felixsun/maslab/BallSortState.java
+++ Derpbot/src/edu/mit/felixsun/maslab/BallSortState.java
@@ -16,36 +16,46 @@ public class BallSortState extends State {
 	double midAngle;
 	double maxAngle;
 	double minAngle;
-
+	BallDetectState detect;
+	boolean toSort = false;
 	public BallSortState(Sensors sensors){
 		res = sensors.photoresistor;
 		led = sensors.led;
 		servo = sensors.sorter;
 		countdown = 0;
+		BallDetectState detect = new BallDetectState(sensors); 
 		midAngle = (servo.getMaxAngle() + servo.getMinAngle())/2;
 		maxAngle = servo.getMaxAngle()-5;
 		minAngle = servo.getMinAngle()+5;
 
 	}
 
-	public int step(int shouldIGo, double greenThresh){
-		/* shouldIGo = 1: ball has been detected, try to sort;
-		 * shouldIGo = 0: no ball; return 0;
-		 */
-		if(shouldIGo==0 && countdown==0){
-			System.out.println("No ball :(");
-			servo.setAngle(midAngle);
-			return 0;
-		}
+	public int step(double ballThresh, double greenThresh){
+		/* toGo = 1: ball has been detected, try to sort;
+		 * toGo = 0: no ball; return 0;
+		 */ 
 		
-		boolean isRed = false;
-		System.out.println(res.getValue());
-		if(res.getValue()<greenThresh){
-			//empirical observation that green does not return false reds.
-			isRed = true;
-		}
-		System.out.println(isRed);
 		if(countdown==0){
+			//If we aren't sorting, check if we need to.
+			if(!toSort){
+				System.out.println("No ball :(");
+				servo.setAngle(midAngle);
+				int toGo = detect.step(ballThresh);
+				if(toGo==1){
+					toSort = true;
+				}
+				return 0;
+			}
+			toSort = false;
+			
+			//If we are sorting, sort.
+			boolean isRed = false;
+			System.out.println(res.getValue());
+			if(res.getValue()<greenThresh){
+				//empirical observation that green does not return false reds.
+				isRed = true;
+			}
+			System.out.println(isRed);
 			//TODO: Move the servos
 			countdown = 20;
 			if(isRed){
@@ -60,6 +70,7 @@ public class BallSortState extends State {
 				servo.setAngle(maxAngle);
 				sorted = true;
 			}
+			led.setValue(false);
 		}
 		else{
 			countdown--;
